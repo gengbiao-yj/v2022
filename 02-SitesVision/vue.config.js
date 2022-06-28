5adb655d803ff9f913a5f440ab7caf5c1478d5e1
gengbiao <898142408@qq.com>
2022-06-28 23:20:37 +0800

    11

--- 02-SitesVision/vue.config.js
+++ 02-SitesVision/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir:`dist_${process.env.NODE_ENV}`, // 设置不同环境的打包输出地址
 });
